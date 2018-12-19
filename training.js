59c08c8b292290481b627db0e17a1dfc3db58762
Christopher Everett <cce29555@gmail.com>
2018-12-19 14:24:39 -0500

    Added everything to master

---
+++ training.js
@@ -0,0 +1,33 @@
+const trainingData = [
+   {
+       input: "Inside Chi's nursery",
+       output: { kardashian: 1 }
+   },{
+       input: "Why I dyed my hair pink",
+       output: { kardashian: 1 }
+   },{
+       input: "Feeling Blue (wearing @kkwbeauty powder contour in medium & dark contour kit as eye shadow, & a new lip coming soon)",
+       output: { kardashian: 1 }
+   },{
+       input: "Remember that you are never alone in your pain, and that everything is temporary. Pain, joy, elation, panic, anxiety, it is all temporary. Be grateful for the experiences that continue to build character, and make you who you are.",
+       output: { kardashian: 1 }
+   },{
+       input: "I will be interviewed by @JudgeJeanine on @FoxNews at 9:00 P.M. Enjoy!",
+       output: { trump: 1 }
+   },{
+       input: "According to NBC News, Voters Nationwide Disapprove of the so-called Mueller Investigation (46%) more than they Approve (41%). You mean they are finally beginning to understand what a disgusting Witch Hunt, led by 17 Angry Democrats, is all about!",
+       output: { trump: 1 }
+   },{
+       input: "“There’s only been 5 times in the last 105 years that an incumbent President has won seats in the Senate in the off year election. Mr. Trump has magic about him. This guy has magic coming out of his ears. He is an astonishing vote getter & campaigner. The Republicans are.........",
+       output: { trump: 1 }
+   },{
+       input: "I will be doing a news conference at The White House - 11:30 A.M. Will be discussing our success in the Midterms!",
+       output: { trump: 1 }
+   },{
+       input: "Dem Memo: FBI did not disclose who the clients were - the Clinton Campaign and the DNC. Wow!",
+       output: { trump: 1 }
+   },{
+       input: "Thank you to the great men and women of the United States @SecretService for a job well done!",
+       output: { trump: 1 }
+   }
+]
